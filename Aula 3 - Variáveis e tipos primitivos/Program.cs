bb5a89f4d5c650f4e8ea83a658555bb4cd474c70
Daniel Amaral <daniel.amaral720@gmail.com>
2020-04-17 19:24:48 -0300

    Aula 3

---
+++ Aula 3 - Variáveis e tipos primitivos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boo = 9>8; //Retorna true  
+            byte byt = 255; //De 0 a 255
+            char cha = '8', cha2 = 'c', cha3 = '!';
+        }
+    }
+}
