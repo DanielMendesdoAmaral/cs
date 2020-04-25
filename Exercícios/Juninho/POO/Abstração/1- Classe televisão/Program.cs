5251917426b941e1a17374d46dfe12d484fb246a
Daniel Amaral <daniel.amaral720@gmail.com>
2020-04-25 20:23:13 -0300

    POO

---
+++ Exercícios/Juninho/POO/Abstração/1- Classe televisão/Program.cs
@@ -0,0 +1,64 @@
+﻿/* 
+
+Implemente uma classe Televisao.
+Atributos:
+- canal (o canal inicial da tv deve ser None)
+- volume (o volume inicial da tv deve ser zero)
+Métodos:
+- aumentar_volume: aumenta o nível de volume em uma unidade
+- diminuir_volume: diminui o nível de volume em uma unidade
+- alterar_canal: recebe o número do canal que será sintonizado e altera o canal da tv
+Faça um programa para criar um objeto Televisao e testar a sua classe. Veja abaixo um trecho de
+programa que utiliza a classe:
+tv = Televisao()
+tv.alterar_canal(5)
+tv.aumentar_volume()
+tv.aumentar_volume()
+tv.aumentar_volume()
+tv.diminuir_volume()
+print('A tv está no canal:', tv.canal) // imprime 5
+print('A tv está no volume:', tv.volume) // imprime 2
+
+*/
+
+using System;
+
+namespace _1__Classe_televisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Televisao tv = new Televisao();
+            tv.alterar_canal(5);
+            tv.aumentar_volume();
+            tv.aumentar_volume();
+            tv.aumentar_volume();
+            tv.diminuir_volume();
+
+            System.Console.WriteLine($"A tv está no canal {tv.canal}");
+            System.Console.WriteLine($"A tv está no volume {tv.volume}");
+        }
+    }
+
+
+    public class Televisao 
+    {
+        public int canal=0;
+        public int volume=0;
+
+        public void aumentar_volume() {
+            volume+=1;
+        }
+
+        public void diminuir_volume() {
+            volume-=1;
+        }
+
+        public int alterar_canal(int c) {
+            canal=c;
+            return canal;
+        }
+
+    }
+}
---
+++ Exercícios/Juninho/POO/Abstração/3- Classe funcionário/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+
+Implemente uma classe Funcionario.
+Atributos:
+- nome
+- salario
+Métodos:
+- aumentar_salario: recebe como parâmetro de entrada um percentual e altera o salário do
+funcionário, de acordo com o percentual recebido.
+Crie um programa que utilize esta classe. Ele deve pedir ao usuário o nome e o salário do
+funcionário e criar um objeto. Depois, deve solicitar ao usuário o percentual de aumento e executar
+o método aumentar salário. Na sequência deve imprimir o salário do funcionário atualizado.
+
+*/
+
+using System;
+
+namespace _3__Classe_funcionário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            System.Console.Write("Informe o nome do funcionário: ");
+            f1.nome = Console.ReadLine();
+            System.Console.Write("Informe o salário atual do funcionário: ");
+            f1.salario = Convert.ToDecimal(Console.ReadLine());
+            System.Console.Write("Informe a porcentagem de aumento do salário que será dado ao funcionário: ");
+            decimal porcentagemAumento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Clear();
+            
+            f1.aumentar_salario(porcentagemAumento);
+        }
+    }
+
+    public class Funcionario {
+        public string nome;
+        public decimal salario;
+        public void aumentar_salario(decimal p) {
+            System.Console.WriteLine($"O novo salário do funcionário é de {salario+=p/100*salario:c}");
+        }
+    }
+}
