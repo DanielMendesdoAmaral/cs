1bf07db3dfdc8deb95122cc05210d007ca9b1323
Daniel Amaral <daniel.amaral720@gmail.com>
2020-04-25 19:13:25 -0300

    1

---
+++ Exercícios/Juninho/POO/Abstração/2- Classe triângulo/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+Crie uma classe que representa um triângulo.
+Atributos:
+- lado_a
+- lado_b
+- lado_c
+Métodos:
+- calcular_perimetro: retorna o perímetro do triângulo (soma dos três lados)
+- maior_lado: retorna o maior lado do triângulo
+Crie um programa que utilize esta classe. O programa deve pedir ao usuário que informe as
+medidas dos três lados de um triângulo. Depois deve criar um objeto com essas medidas e exibir
+seu perímetro e seu maior lado.
+
+*/
+
+using System;
+
+namespace _2__Classe_triângulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
