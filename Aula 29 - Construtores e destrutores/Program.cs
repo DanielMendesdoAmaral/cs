fecd7ca320e9aaea36be7c518578129c780ed1b0
Daniel Amaral <daniel.amaral720@gmail.com>
2020-04-25 22:48:31 -0300

    5

---
+++ Aula 29 - Construtores e destrutores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Aula_29___Construtores_e_destrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Digite o nome do jogo 1: ");
+            string nome = Console.ReadLine();
+            System.Console.Write("Digite o gênero do jogo 1: ");
+            string genero = Console.ReadLine();
+            System.Console.Write("Digite a data de lançamento do jogo 1: ");
+            int lancamento = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            
+            Games j1 = new Games(nome, genero, lancamento);
+        }
+    }
+
+    public class Games 
+    {
+        public string nome;
+        public string genero;
+        public int lancamento;
+        
+        //Um construtor é um método que inicializa os atributos. Pode se inicializar fora dele, mas não é o recomendado. Pode passar parâmetros a ele também, assim obriga o usuário digitar valores seguindo determinadas regras. O método construtor tem que ter o mesmo nome da classe, e não pode ter nenhum tipo de retorno.
+
+        public Games(string n, string g, int l) {
+            System.Console.WriteLine($"JOGO: {nome=n} \nGÊNERO: {genero=g} \nDATA DE LANÇAMENTO: {lancamento=l}");
+        }
+
+        //Um destrutor é aquele que é executado após a execução do programa. Tem que ter um ~ antes, e não precisa da visibilidade do método. Precisa ter o mesmo nome do método. Serve para limpar a memória após a execução, útil em sistemas robustos.
+
+        ~ Games () {
+            Console.WriteLine($"Jogo {nome} destruído.");
+        }
+    }
+}
---
+++ Exercícios/Juninho/POO/Abstração/5- Classe carro/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+
+Implemente uma classe Carro
+Propriedades:
+- consumo (medidos em km/litro): O consumo deve ser especificado no construtor.
+- quantidade_combustivel (quantidade de litros de combustível no tanque do carro): a
+quantidade inicial deve ser zero
+Métodos:
+- adicionar_combustivel: recebe uma quantidade de litros de combustível para abastecer o
+tanque.
+- obter_combustivel: retorna a quantidade atual de combustível.
+- andar: recebe uma distância em km e simula o ato de dirigir o veículo por essa distância,
+reduzindo o nível de combustível no tanque de gasolina (de acordo com o consumo do
+veículo)
+Faça um programa para testar a classe Carro. Veja abaixo um trecho de programa que utiliza a
+classe:
+fusca = Carro(15) // 15 quilômetros por litro de combustível
+fusca.adicionar_combustivel(20) // abastece com 20 litros de combustível
+fusca.andar(100) // anda 100 quilômetros.
+print(fusca.obter_combustivel()) // imprime o combustível que resta no tanque
+
+*/
+
+using System;
+
+namespace _5__Classe_carro
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Carro bmw = new Carro(15);
+
+            bmw.AdicionarCombustivel(20);
+            bmw.Andar(100);
+            bmw.ObterCombustivel();
+        }
+    }
+
+    public class Carro 
+    {
+        int consumo;
+        int quantidadeCombustivel; 
+
+        public Carro(int kml) {
+            consumo=kml;
+            quantidadeCombustivel=0;
+        }
+
+        public void AdicionarCombustivel(int addc) {
+            quantidadeCombustivel+=addc;
+        }
+
+        public void Andar(int dist) {
+            dist=dist/consumo;
+            quantidadeCombustivel-=dist;
+        }
+
+        public void ObterCombustivel() {
+            System.Console.WriteLine(quantidadeCombustivel);
+        }
+    }
+}
