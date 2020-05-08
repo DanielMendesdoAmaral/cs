0bb1f5eda4ede5eec57d43a6a43643f90eaba076
Daniel Amaral <daniel.amaral720@gmail.com>
2020-05-08 15:15:31 -0300

    final herança

---
+++ Exercícios/Juninho/POO/Herança/5- Um veículo TEM UM motor, carro e caminhão SÃO veículos/Program.cs
@@ -0,0 +1,154 @@
+﻿/*
+
+Crie a classe Motor que contém cilindrada e potencia.
+Crie a classe Veiculo contendo ano de fabricação, preco e motor. Crie também o metodo exibir_dados para mostrar os dados do Veículo.
+Crie a classe Carro, que herda da classe Veiculo e inclua os atributos cor e modelo. Crie também o metodo exibir_dados para mostrar os dados do Carro.
+Crie a classe Caminhão, que herda da classe Veiculo e inclua o atributos comprimento (em metros). Crie também o metodo exibir_dados para mostrar os dados do Caminhão.
+
+Utilize o programa abaixo para testar as classes
+motor1 = Motor(1000, 500)
+motor2 = Motor(8000, 900)
+carro = Carro(2010, 20000, motor1, "branca", "gol")
+caminhao = Caminhao(2015, 80000, motor2, 10)
+carro.exibir_dados() # imprime os valores de todos os atributos do carro
+caminhao.exibir_dados() # imprime os valores de todos os atributos do caminhão 
+
+*/
+
+using System;
+
+namespace _5__Um_veículo_TEM_UM_motor__carro_e_caminhão_SÃO_veículos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor motor1 = new Motor(1000, 500);
+            Motor motor2 = new Motor(8000, 900);
+            Carro carro = new Carro(2020, 20000, motor1, "Branco", "BMW");
+            Caminhao caminhao = new Caminhao(2015, 80000, motor2, 10);
+
+            carro.ExibirDados();
+            caminhao.ExibirDados();
+        }
+    }
+
+    public class Motor {
+        private int cilindrada;
+        private int potencia;
+
+        public Motor(int c, int p) {
+            SetCilindrada(c);
+            SetPotencia(p);
+        }
+
+        public int GetCilindrada() {
+            return cilindrada;
+        }
+        public int GetPotencia() {
+            return potencia;
+        }
+
+        public void SetCilindrada(int c) {
+            cilindrada=c;
+        }
+        public void SetPotencia(int p) {
+            potencia=p;
+        }
+    }
+
+    public interface IVeiculo {
+        void ExibirDados();
+    }
+
+    public class Veiculo : IVeiculo {
+        private int ano;
+        private double preco;
+        private Motor motor;
+
+        public Veiculo(int a, double p, Motor m) {
+            SetAno(a);
+            SetPreco(p);
+            SetMotor(m);
+        }
+
+        public int GetAno() {
+            return ano;
+        }
+        public double GetPreco() {
+            return preco;
+        }
+        public Motor GetMotor() {
+            return motor;
+        }
+
+        public void SetAno(int a) {
+            ano=a;
+        }
+        public void SetPreco(double p) {
+            preco=p;
+        }
+        public void SetMotor(Motor m) {
+            motor=m;
+        }
+
+        virtual public void ExibirDados() {
+            System.Console.Write($"ANO: {GetAno()}\nPREÇO: {GetPreco()}\nCARACTERÍSTICAS DO MOTOR\nCILINDRADA: {motor.GetCilindrada()}\nPOTÊNCIA: {motor.GetPotencia()}\n\n");
+        }
+    }
+
+    public interface ICarro {
+        void ExibirDados();
+    }
+
+    public class Carro : Veiculo, ICarro {
+        private string cor;
+        private string modelo;
+
+        public Carro(int a, double p, Motor m, string c, string model) : base(a, p, m) {
+            SetCor(c);
+            SetModelo(model);
+        }
+
+        public string GetCor() {
+            return cor;
+        }
+        public string GetModelo() {
+            return modelo;
+        }
+
+        public void SetCor(string c) {
+            cor=c;
+        }
+        public void SetModelo(string m) {
+            modelo=m;
+        }
+
+        override public void ExibirDados() {
+            System.Console.Write($"ANO: {GetAno()}\nPREÇO: {GetPreco()}\nCARACTERÍSTICAS DO MOTOR\nCILINDRADA: {GetMotor().GetCilindrada()}\nPOTÊNCIA: {GetMotor().GetPotencia()}\nCOR: {GetCor()}\nMODELO: {GetModelo()}\n\n");
+        }
+    }
+
+    public interface ICaminhao {
+        void ExibirDados();
+    }
+
+    public class Caminhao : Veiculo, ICaminhao {
+        private double comprimento;
+
+        public Caminhao(int a, double p, Motor m, double c) : base(a, p, m) {
+            SetComprimento(c);
+        }
+
+        public double GetComprimento() {
+            return comprimento;
+        }
+        public void SetComprimento(double c) {
+            comprimento=c;
+        }
+
+        override public void ExibirDados() {
+            System.Console.Write($"ANO: {GetAno()}\nPREÇO: {GetPreco()}\nCARACTERÍSTICAS DO MOTOR\nCILINDRADA: {GetMotor().GetCilindrada()}\nPOTÊNCIA: {GetMotor().GetPotencia()}\nCOMPRIMENTO: {GetComprimento()}\n\n");
+        }
+    }
+}
---
+++ Exercícios/Juninho/POO/Herança/6- DESAFIO FINAL DE HERANÇA/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+
+Uma universidade necessita de um sistema que facilite a sua gestão acadêmica. Implemente as classes de modo que obedeçam os relacionamentos apresentados no diagrama abaixo:
+
+Sabe-se que um professor é um funcionário. Além de professores, há funcionários que são técnicos administrativos.
+Para cada funcionário, independente de ser professor ou técnico, é necessário saber seu nome, endereço, telefone, cpf e salário.
+Especificamente para professores, é necessário saber sua titulação e as disciplinas que ele leciona.
+Especificamente para técnicos administrativos, é necessário saber seu cargo.
+Para cada aluno é necessário saber seu nome, endereco, telefone, cpf e as disciplinas que ele cursa.
+Para cada disciplina é necessário registrar seu nome.
+
+*/
+
+using System;
+
+namespace _6__DESAFIO_FINAL_DE_HERANÇA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
