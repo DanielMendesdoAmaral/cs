4017550ca87c8a2799efad3ad6cc3026822122b8
Daniel Amaral <daniel.amaral720@gmail.com>
2020-05-30 23:57:11 -0300

    54

---
+++ Aula 54 - Namespaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula_54___Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperaçoesMatematicas.OperacaoUm o = new OperaçoesMatematicas.OperacaoUm(8, 2); //Utilizamos outro namespace. Precisamos informar, ou usar um using. No caso, System é um namespace.
+            Console.WriteLine( o.Soma() );
+        }
+    }
+}
+
+namespace OperaçoesMatematicas {
+    public class OperacaoUm {
+        double n1;
+        double n2;
+
+        public OperacaoUm(double a, double b) {
+            n1=a;
+            n2=b;
+        }
+
+        public double Soma() {
+            return n1+n2;
+        }
+    }
+}
