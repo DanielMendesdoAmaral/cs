dcec6527fd72902395d6d8c2f571fd796fb21d68
Daniel Amaral <daniel.amaral720@gmail.com>
2020-05-30 23:48:09 -0300

    Aula 53

---
+++ Aula 51 - Argumentos de entrada/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula_51___Argumentos_de_entrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //O parâmetro de entrada args é inserido junto com a inicialização do programa, ou seja: dotnet run a b c d qualquer parametro.
+
+            for (int c=0; c<args.Length; c++) {
+                System.Console.WriteLine(args[c]);
+            }
+        }
+    }
+}
